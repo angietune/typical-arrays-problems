ef62ba57db50b55b1dae4af1f0729d0279612cd9
angietune <angietune@yandex.ru>
2021-03-04 09:34:01 +0300

    init: Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array == null || array.length == 0) {
+    return 0;
+  } else {
+    return Math.min.apply(Math, array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array == null || array.length == 0) {
+    return 0;
+  } else {
+  return Math.max.apply(Math, array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array == null || array.length == 0) {
+    return 0;
+  } else {
+  let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += parseInt(array[i], 10);
+    }
+  return sum/array.length;
+  }
 }
